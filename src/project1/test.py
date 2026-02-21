1419282446f11367c9d3bbfa6b7b96fd53d3a850
Vivek Agrawal <1678871+vmnit@users.noreply.github.com>
2026-02-21 10:52:33 -0600

    starts project1

---
+++ src/project1/test.py
@@ -0,0 +1,28 @@
+import gym
+from stable_baselines3 import A2C
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.env_util import make_atari_env
+import os
+
+environment_name = "Breakout-v0"
+env = gym.make(environment_name)
+
+
+
+episodes = 5
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0 
+    
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score+=reward
+    print('Episode:{} Score:{}'.format(episode, score))
+env.close()
+
+env.action_space.sample()
+env.observation_space.sample()
---
+++ src/project1/trainModel.py
@@ -0,0 +1,10 @@
+import gym 
+from stable_baselines3 import A2C
+from stable_baselines3.common.vec_env import VecFrameStack
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.env_util import make_atari_env
+import os
+
+environment_name = "Breakout-v0"
+env = gym.make(environment_name)
+
